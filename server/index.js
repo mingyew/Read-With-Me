bf409c2963873b7eb112eb71a31cbe917db39f21
mingyew <lee_ming_yew@hotmail.com>
2022-06-20 16:59:56 +0100

    Deplay to heroku

--- server/index.js
+++ server/index.js
@@ -13,6 +13,7 @@ res.json({ message: "Hello from server!" });
 
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
 
 app.listen(PORT, () => {
 console.log(`Server listening on ${PORT}`);
