cde408a9380913195a56218b655f81283d40ee50
mingyew <lee_ming_yew@hotmail.com>
2022-08-02 10:41:00 +0100

    login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,33 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthContext";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import Storypage from "./pages/Storypage";
+import StoryPage from "./pages/StoryPage";
 import Signup from "./pages/Signup";
 import Stories from "./pages/Stories";
+import Login from "./pages/Login";
+import PrivateRoute from "./pages/PrivateRoute";
+import Dashboard from "./pages/Dashboard";
+import ForgotPassword from "./pages/ForgotPassword";
 
 function App() {
+  const isAuthenticated = false;
+
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Stories />}></Route>
-        <Route path="/signup" element={<Signup />}></Route>
-        <Route path="/story/:id" element={<Storypage />}></Route>
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route exact path="/" element={<PrivateRoute />}>
+            <Route exact path="/" element={<Dashboard />} />
+          </Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/stories" element={<Stories />}></Route>
+          <Route path="/signup" element={<Signup />}></Route>
+          <Route path="/story/:id" element={<StoryPage />}></Route>
+          <Route path="/forgot-password" element={<ForgotPassword />}></Route>
+        </Routes>
+      </AuthProvider>
     </Router>
   );
 }
